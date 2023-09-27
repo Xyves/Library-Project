10ec7bcb052f2b6d78a1b96acf64169d338655c4
Xyves <xodrixx@protonmail.com>
2023-09-27 13:15:58 +0200

    Swap a function for class

--- script.js
+++ script.js
@@ -8,24 +8,37 @@ document.addEventListener("DOMContentLoaded", function () {
     event.preventDefault();
     saveUserData();
   });
-
+  class createData {
+    constructor(bookName, bookAuthor, bookPages, bookRead) {
+      this.bookName = bookName;
+      this.bookAuthor = bookAuthor;
+      this.bookPages = bookPages;
+      this.bookRead = bookRead;
+    }
+  }
   function saveUserData() {
     const bookName = document.querySelector("#bookName").value;
     const bookAuthor = document.querySelector("#author").value;
     const bookPages = document.querySelector("#book-pages").value;
     const bookRead = document.querySelector("#read").checked;
-
-    const formData = {
-      bookName: bookName,
-      author: bookAuthor,
-      pages: bookPages,
-      read: bookRead,
-    };
-
-    addBook(formData);
+    const formData = new createData(bookName, bookAuthor, bookPages, bookRead);
+    console.log(formData);
+    if (
+      bookName !== "" &&
+      bookAuthor !== "" &&
+      bookPages !== "" &&
+      bookRead !== ""
+    ) {
+      addBook(formData);
+    } else {
+      document.querySelector("#bookName").placeholder = "Update the book name!";
+      document.querySelector("#author").placeholder = "Update the author!";
+      document.querySelector("#book-pages").placeholder =
+        "Update the number of pages!";
+    }
   }
 });
-let cardCount = 1;
+let cardCount = 4;
 function addBook(formData) {
   const template = document.querySelector("#myTemplate");
   const clone = document.importNode(template.content, true);
@@ -33,7 +46,7 @@ function addBook(formData) {
   const topHeader = clone.querySelector(".topHeader");
   const bottomCard = clone.querySelector(".bottomCard");
   const input = bottomCard.querySelector("input[type='checkbox']");
-  // Input
+
   // Generate inique ID
   const inputId = `cardRead${cardCount}`;
   const labelFor = inputId;
@@ -44,11 +57,11 @@ function addBook(formData) {
   const label = card.querySelector("label");
   label.setAttribute("for", labelFor);
 
-  input.checked = formData.read;
+  input.checked = formData.bookRead;
   topHeader.querySelector("h2").textContent = `${formData.bookName}`;
-  topHeader.querySelector(".bookAuthor").textContent = `${formData.author}`;
+  topHeader.querySelector(".bookAuthor").textContent = `${formData.bookAuthor}`;
   topHeader.querySelector(".pages").textContent = `${
-    formData.pages + " " + "pages"
+    formData.bookPages + " " + "pages"
   }`;
   bookList.appendChild(clone);
 
